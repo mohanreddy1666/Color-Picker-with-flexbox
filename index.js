454d737a0e986943c22638665dec72e3d6ccb509
mohanreddy1666 <mohannallamilli11@gmail.com>
2021-10-25 18:13:58 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+/* 
+    Create a variable to store the count of colors to be loaded
+    
+    Update the getColors() function to take that count as a parameter
+    
+    Set the count to 100 colors, update the .my-color class to maintain the grid structure
+*/
+
+function displayColors(colors) {
+    let myColorsHtml = colors.map(color => {
+        return `<div class="my-color" style="background-color: ${color.value}"></div>`
+    }).join('')
+    
+    console.log(myColorsHtml)
+
+    document.body.innerHTML = `<div class="my-colors">
+        ${myColorsHtml}
+    </div>`
+}
+
+async function getColors(colorCount) {
+    let response = await fetch(`https://api.noopschallenge.com/hexbot?count=${colorCount}`)
+    let data = await response.json()
+    let colors = data.colors
+    
+    displayColors(colors)
+}
+
+let colorCount = 100
+getColors(colorCount)
